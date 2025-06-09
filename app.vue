e95dfa02766032cde2192535b528388d5eab548b
Pavle Jovanovic <44483259+minzhoudu@users.noreply.github.com>
2025-06-09 13:40:24 +0200

    Tl#4 daisyui setup (#4)
    * daisyui package install
    
    * daisy ui setup

--- app.vue
+++ app.vue
@@ -4,5 +4,9 @@
     <h1 class="text-3xl font-bold underline rounded-2xl flex">
       Hello World
     </h1>
+
+    <button class="btn btn-secondary">
+      Click me
+    </button>
   </div>
 </template>
