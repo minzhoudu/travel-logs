f6ddd39188ecbacd0f180a67a5b43d1597cea173
Pavle Jovanovic <44483259+minzhoudu@users.noreply.github.com>
2025-06-09 13:31:24 +0200

    tailwindcss setup (#3)

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <h1>Hello World</h1>
+    <h1 class="text-3xl font-bold underline rounded-2xl flex">
+      Hello World
+    </h1>
   </div>
 </template>
