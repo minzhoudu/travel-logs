53a25eae1c5f5e985a6016a10f32bb87dc92288e
Pavle Jovanovic <44483259+minzhoudu@users.noreply.github.com>
2025-06-14 22:06:45 +0200

    tl#12 show auth status in ui + logout (#12)
    * tl#12 show auth status in ui + logout
    
    * sign out icon size bump up

---
+++ components/app/dropdown/dropdown.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="dropdown dropdown-end dropdown-hover">
+    <div tabindex="0" role="button" class="btn m-1">
+      <slot name="placeholder" />
+    </div>
+
+    <ul tabindex="0" class="dropdown-content menu bg-base-200 rounded-box z-1 w-52 p-2 shadow-sm">
+      <slot />
+    </ul>
+  </div>
+</template>
