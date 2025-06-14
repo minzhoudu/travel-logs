80e3b1d3c1f8eff72deb710801f37e674ac68777
Pavle <pavle.jovanovic@source.ag>
2025-06-14 17:06:18 +0200

    tl#9 drizzle turso sqlite setup

--- lib/env.ts
+++ lib/env.ts
@@ -4,8 +4,13 @@ import tryParseEnv from "./utils/try-parse-env";
 
 const EnvSchema = z.object({
   NODE_ENV: z.enum(["development", "production"]),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_AUTH_TOKEN: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
 
-export default tryParseEnv(EnvSchema);
+tryParseEnv(EnvSchema);
+
+// eslint-disable-next-line node/no-process-env
+export default EnvSchema.parse(process.env);
